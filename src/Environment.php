e6197fb3afdf9fb7266b0795b51138cf680b8270
Leo <losoviz@gmail.com>
2019-11-23 17:09:12 +0800

    Typed functions

--- src/Environment.php
+++ src/Environment.php
@@ -3,7 +3,7 @@ namespace PoP\GraphQLAPI;
 
 class Environment
 {
-    public static function disableGraphQLAPI()
+    public static function disableGraphQLAPI(): bool
     {
         return isset($_ENV['DISABLE_GRAPHQL_API']) ? strtolower($_ENV['DISABLE_GRAPHQL_API']) == "true" : false;
     }
