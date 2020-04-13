e1a3ab706b3d13bc1f2d9ddec046f503d035df38
Leo <losoviz@gmail.com>
2020-04-13 19:07:41 +0800

    Added declare(strict_types=1);

--- src/Component.php
+++ src/Component.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 namespace PoP\GraphQLAPI;
 
 use PoP\API\Component as APIComponent;
--- src/Config/ServiceConfiguration.php
+++ src/Config/ServiceConfiguration.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 namespace PoP\GraphQLAPI\Config;
 
 use PoP\ComponentModel\Container\ContainerBuilderUtils;
